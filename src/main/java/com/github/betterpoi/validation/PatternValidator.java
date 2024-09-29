a48a3906f903fc65e308eb6b251cec9e7d7e23eb
r4tylmz <ahmedyjlmaz@gmail.com>
2024-09-29 23:16:50 +0300

    fix: Fix PatternValidator and RequiredValidator logic

--- src/main/java/com/github/betterpoi/validation/PatternValidator.java
+++ src/main/java/com/github/betterpoi/validation/PatternValidator.java
@@ -8,13 +8,13 @@ public class PatternValidator implements CellValidator {
     public String validate(CellHolder cellHolder) {
         final String cellValue = cellHolder.getCellValue();
         final String pattern = cellHolder.getBpColumn().pattern();
-        if (cellValue == null || pattern.isEmpty()) {
+        if (cellValue == null || pattern == null || pattern.isEmpty()) {
             return null;
         }
         final Pattern validatorPattern = Pattern.compile(pattern, Pattern.CASE_INSENSITIVE);
         final Matcher matcher = validatorPattern.matcher(cellValue);
         if (!matcher.find()) {
-            return String.format("Cell value is not valid. Cell value must match with the pattern : %s", pattern);
+            return String.format("Cell value [%s] is not valid. Cell value must match with the pattern : %s", cellValue, pattern);
         }
         return null;
     }
--- src/main/java/com/github/betterpoi/validation/RequiredValidator.java
+++ src/main/java/com/github/betterpoi/validation/RequiredValidator.java
@@ -7,7 +7,7 @@ public class RequiredValidator implements CellValidator {
     public String validate(CellHolder cellHolder) {
         String cellValue = cellHolder.getCellValue();
         if (!cellHolder.getBpColumn().required()) return null;
-        if (cellValue == null || cellValue.isEmpty()) return null;
-        return String.format("Column %s is required", ColUtil.getHeaderTitle(cellHolder.getBpColumn()));
+        if (cellValue != null && !cellValue.isEmpty()) return null;
+        return String.format("The column with the header [%s] is required and cannot be null or empty.", ColUtil.getHeaderTitle(cellHolder.getBpColumn()));
     }
 }
